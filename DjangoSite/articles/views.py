c5dc35a32b31edc485f1e761ac49d16685d2f69e
K3nny13 <kenneth_cogzell@hotmail.com>
2018-10-01 21:44:55 +0200

    deleted images

--- DjangoSite/articles/views.py
+++ DjangoSite/articles/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, HttpResponse
 from . models import Article
 
 # Create your views here.
+
+
 def article_list(request):
     articles = Article.objects.all().order_by('-date')
-    return render(request, 'articles/article_list.html', context = {'articles': articles})
+    return render(request, 'articles/article_list.html', context={'articles': articles})
+
 
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
-    return render(request, 'articles/article_detail.html', context = {'article': article})
+    return render(request, 'articles/article_detail.html', context={'article': article})
